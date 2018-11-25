130a7eec0e48095e3849b917382f44ce21ededfa
sizer <shinse.tanaka@gmail.com>
2018-11-26 08:41:42 +0900

    add func fetchRfcDetail

--- main.go
+++ main.go
@@ -2,8 +2,27 @@
 // Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+	fmt.Println("main started.")
+	fetchRfcDetail("8493")
+	fmt.Println("main finished.")
+}
+
+func fetchRfcDetail(rfcNo string) {
+	res, err := http.Get("https://www.rfc-editor.org/rfc/rfc" + rfcNo + ".txt")
+	if err != nil {
+		fmt.Println("find error." + err.Error())
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s", b)
 }
