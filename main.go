bb8f14ccef5297d4026e03d94c0af4bda65a439b
sizer <shinse.tanaka@gmail.com>
2018-11-26 11:24:42 +0900

    add func makeCache & readCache

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
 import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -15,6 +14,16 @@ func main() {
 }
 
 func fetchRfcDetail(rfcNo string) {
+	var cacheName = "RFC" + rfcNo + ".txt"
+
+	if checkIfCached(cacheName) {
+		readCache(cacheName)
+	} else {
+		httpGetRfcDetail(rfcNo)
+	}
+}
+
+func httpGetRfcDetail(rfcNo string) string {
 	res, err := http.Get("https://www.rfc-editor.org/rfc/rfc" + rfcNo + ".txt")
 	if err != nil {
 		fmt.Println("find error." + err.Error())
@@ -25,4 +34,30 @@ func fetchRfcDetail(rfcNo string) {
 		fmt.Println(err)
 	}
 	fmt.Printf("%s", b)
+	
+	makeCache("RFC" + rfcNo + ".txt", b)
+	return string(b)
+}
+
+func makeCache(filename string, data []byte) {
+	ioutil.WriteFile(getCacheFilePath(filename), data, 0644)	
+}
+
+func readCache(cacheName string) {
+	b, err := ioutil.ReadFile(getCacheFilePath(cacheName))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s", b)
+}
+
+func checkIfCached(key string) bool {
+	if _, err := os.Stat(getCacheFilePath(key)); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func getCacheFilePath(filename string) string {
+	return "tmp/cache/" + filename
 }
