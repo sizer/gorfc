41627106c6021869f392d6e871346635dd091acf
sizer <shinse.tanaka@gmail.com>
2018-11-27 12:03:57 +0900

    refactor/ simplify httpGetRfcDetail

--- fetch/RfcDetail.go
+++ fetch/RfcDetail.go
@@ -2,6 +2,7 @@ package fetch
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -14,23 +15,25 @@ func RfcDetail(rfcNo string) {
 	if checkIfCached(cacheName) {
 		readCache(cacheName)
 	} else {
-		httpGetRfcDetail(rfcNo)
+		res, _ := httpGetRfcDetail(rfcNo)
+		b, err := ioutil.ReadAll(res)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%s", b)
+
+		makeCache(cacheName, b)
 	}
 }
 
-func httpGetRfcDetail(rfcNo string) {
+func httpGetRfcDetail(rfcNo string) (io.ReadCloser, error) {
 	res, err := http.Get("https://www.rfc-editor.org/rfc/rfc" + rfcNo + ".txt")
 	if err != nil {
 		fmt.Println("find error." + err.Error())
+		return nil, err
 	}
 
-	b, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("%s", b)
-	
-	makeCache(getCacheName(rfcNo), b)
+	return res.Body, nil
 }
 
 func getCacheName(rfcNo string) string {
